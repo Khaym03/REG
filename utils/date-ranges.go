cf573eece0cbf5d9f32f73cee8282c9c24736f1e
Khaym03 <daniellarosa20003@gmail.com>
2026-03-27 07:07:11 -0400

    refactor MonthlyDateRanges

--- utils/date-ranges.go
+++ utils/date-ranges.go
@@ -7,6 +7,29 @@ type DateRange struct {
 	To   time.Time
 }
 
+func MonthlyDateRanges(from, to time.Time) []DateRange {
+	var ranges []DateRange
+
+	// Normalize to first day of month
+	current := time.Date(from.Year(), from.Month(), 1, 0, 0, 0, 0, from.Location())
+
+	end := time.Date(to.Year(), to.Month(), 1, 0, 0, 0, 0, to.Location())
+
+	for !current.After(end) {
+		firstDay := current
+		lastDay := current.AddDate(0, 1, -1)
+
+		ranges = append(ranges, DateRange{
+			From: firstDay,
+			To:   lastDay,
+		})
+
+		current = current.AddDate(0, 1, 0)
+	}
+
+	return ranges
+}
+
 func MonthlyDateRangesCurrentToLastYear() []DateRange {
 	var dateSet []DateRange
 
