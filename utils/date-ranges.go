f9ad57c8db3150b8b47797fcb48544be42f37935
Khaym03 <daniellarosa20003@gmail.com>
2026-03-28 22:18:03 -0400

    delete unused code MonthlyDateRangesCurrentToLastYear

--- utils/date-ranges.go
+++ utils/date-ranges.go
@@ -37,29 +37,3 @@ func MonthlyDateRanges(from, to time.Time) []DateRange {
 
 	return ranges
 }
-
-func MonthlyDateRangesCurrentToLastYear() []DateRange {
-	var dateSet []DateRange
-
-	now := time.Now()
-
-	// Loop to generate the set from current month back to last year
-	for i := 0; i <= 12; i++ {
-		// Calculate the month relative to now
-		targetDate := now.AddDate(0, -i, 0)
-
-		// Get the 1st day of that month
-		firstDay := time.Date(targetDate.Year(), targetDate.Month(), 1, 0, 0, 0, 0, targetDate.Location())
-
-		// Get the last day (1st day of next month minus 1 day)
-		lastDay := firstDay.AddDate(0, 1, -1)
-
-		// Append to our set
-		dateSet = append(dateSet, DateRange{
-			From: firstDay,
-			To:   lastDay,
-		})
-	}
-
-	return dateSet
-}
