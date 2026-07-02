b7fad4479a43845603514217b864b5a2d31ec52b
Khaym03 <daniellarosa20003@gmail.com>
2026-07-02 14:57:32 -0400

    mediator/session: add mediator files and update session management

--- internal/auth/authenticated-session.go
+++ internal/auth/authenticated-session.go
@@ -1,54 +1,54 @@
 package auth
 
-import (
-	"context"
+// import (
+// 	"context"
 
-	"github.com/Khaym03/REG/internal/event"
-	"github.com/mustafaturan/bus/v3"
-	log "github.com/sirupsen/logrus"
-)
+// 	"github.com/Khaym03/REG/internal/event"
+// 	"github.com/mustafaturan/bus/v3"
+// 	log "github.com/sirupsen/logrus"
+// )
 
-var _ Session = (*AuthenticatedSession)(nil)
+// var _ Session = (*AuthenticatedSession)(nil)
 
-type AuthenticatedSession struct {
-	base Session
-	auth AuthService
-	user User
+// type AuthenticatedSession struct {
+// 	base Session
+// 	auth AuthService
+// 	user User
 
-	eventBus *bus.Bus
-}
+// 	eventBus *bus.Bus
+// }
 
-func NewAuthenticatedSession(
-	ctx context.Context,
-	base Session,
-	auth AuthService,
-	user User,
-	eventBus *bus.Bus,
-) (*AuthenticatedSession, error) {
-	if err := eventBus.Emit(ctx, string(event.Login), struct{}{}); err != nil {
-		log.Error(err)
-	}
+// func NewAuthenticatedSession(
+// 	ctx context.Context,
+// 	base Session,
+// 	auth AuthService,
+// 	user User,
+// 	eventBus *bus.Bus,
+// ) (*AuthenticatedSession, error) {
+// 	if err := eventBus.Emit(ctx, string(event.Login), struct{}{}); err != nil {
+// 		log.Error(err)
+// 	}
 
-	if err := auth.Login(ctx, base, user); err != nil {
-		return nil, err
-	}
+// 	if err := auth.Login(ctx, base, user); err != nil {
+// 		return nil, err
+// 	}
 
-	return &AuthenticatedSession{
-		base:     base,
-		auth:     auth,
-		eventBus: eventBus,
-	}, nil
-}
+// 	return &AuthenticatedSession{
+// 		base:     base,
+// 		auth:     auth,
+// 		eventBus: eventBus,
+// 	}, nil
+// }
 
-func (s *AuthenticatedSession) Do(ctx context.Context, fn PageFunc) error {
-	return s.base.Do(ctx, fn)
-}
+// func (s *AuthenticatedSession) Do(ctx context.Context, fn PageFunc) error {
+// 	return s.base.Do(ctx, fn)
+// }
 
-func (s *AuthenticatedSession) NewIsolated(ctx context.Context) (Session, error) {
-	return s.base.NewIsolated(ctx)
-}
+// func (s *AuthenticatedSession) NewIsolated(ctx context.Context) (Session, error) {
+// 	return s.base.NewIsolated(ctx)
+// }
 
-func (s *AuthenticatedSession) Close(ctx context.Context) error {
+// func (s *AuthenticatedSession) Close(ctx context.Context) error {
 
-	return s.base.Close(ctx)
-}
+// 	return s.base.Close(ctx)
+// }
--- internal/auth/session-manager.go
+++ internal/auth/session-manager.go
@@ -1,237 +1,237 @@
 package auth
 
-import (
-	"context"
-	"io"
-	"sync"
+// import (
+// 	"context"
+// 	"io"
+// 	"sync"
 
-	"github.com/Khaym03/REG/internal/browser"
-	"github.com/Khaym03/REG/internal/config"
-	"github.com/go-rod/rod"
-	"github.com/mustafaturan/bus/v3"
-	log "github.com/sirupsen/logrus"
-	"github.com/wailsapp/wails/v3/pkg/application"
-)
+// 	"github.com/Khaym03/REG/internal/browser"
+// 	"github.com/Khaym03/REG/internal/config"
+// 	"github.com/go-rod/rod"
+// 	"github.com/mustafaturan/bus/v3"
+// 	log "github.com/sirupsen/logrus"
+// 	"github.com/wailsapp/wails/v3/pkg/application"
+// )
 
-type SessionProvider interface {
-	Session(context.Context) (Session, error)
-}
+// type SessionProvider interface {
+// 	Session(context.Context) (Session, error)
+// }
 
-type Reconfigurer interface {
-	Reconfigure(ctx context.Context, cfg config.BrowserConfig) error
-}
+// type Reconfigurer interface {
+// 	Reconfigure(ctx context.Context, cfg config.BrowserConfig) error
+// }
 
-type SessionManager interface {
-	SessionProvider
+// type SessionManager interface {
+// 	SessionProvider
 
-	Reconfigurer
+// 	Reconfigurer
 
-	Init(context.Context) error
-	Login(context.Context, User) error
-	Logout(context.Context) error
+// 	Init(context.Context) error
+// 	Login(context.Context, User) error
+// 	Logout(context.Context) error
 
-	io.Closer
-}
+// 	io.Closer
+// }
 
-type sessionManager struct {
-	eventBus *bus.Bus
-	service  AuthService
+// type sessionManager struct {
+// 	eventBus *bus.Bus
+// 	service  AuthService
 
-	browser     *rod.Browser
-	browserConf config.BrowserConfig
-	stateMu     sync.Mutex
+// 	browser     *rod.Browser
+// 	browserConf config.BrowserConfig
+// 	stateMu     sync.Mutex
 
-	ready chan struct{}
+// 	ready chan struct{}
 
-	initOnce sync.Once
-	initErr  error
+// 	initOnce sync.Once
+// 	initErr  error
 
-	sessionMu sync.RWMutex
-	session   Session
-}
+// 	sessionMu sync.RWMutex
+// 	session   Session
+// }
 
-func NewSessionManager(eventBus *bus.Bus) *sessionManager {
-	return &sessionManager{
-		eventBus:    eventBus,
-		service:     NewLoginScraper(),
-		ready:       make(chan struct{}),
-		stateMu:     sync.Mutex{},
-		browserConf: config.BrowserConfFromENV(),
-	}
-}
+// func NewSessionManager(eventBus *bus.Bus) *sessionManager {
+// 	return &sessionManager{
+// 		eventBus:    eventBus,
+// 		service:     NewLoginScraper(),
+// 		ready:       make(chan struct{}),
+// 		stateMu:     sync.Mutex{},
+// 		browserConf: config.BrowserConfFromENV(),
+// 	}
+// }
 
-func (s *sessionManager) ServiceStartup(
-	ctx context.Context,
-	_ application.ServiceOptions,
-) error {
+// func (s *sessionManager) ServiceStartup(
+// 	ctx context.Context,
+// 	_ application.ServiceOptions,
+// ) error {
 
-	go func() {
-		err := s.Init(ctx)
-		if err != nil {
-			log.Error(err)
-		}
-	}()
+// 	go func() {
+// 		err := s.Init(ctx)
+// 		if err != nil {
+// 			log.Error(err)
+// 		}
+// 	}()
 
-	return nil
-}
+// 	return nil
+// }
 
-func (s *sessionManager) ServiceShutdown() error {
-	err := s.Close()
-	if err != nil {
-		log.Error(err)
-	}
+// func (s *sessionManager) ServiceShutdown() error {
+// 	err := s.Close()
+// 	if err != nil {
+// 		log.Error(err)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (s *sessionManager) Init(ctx context.Context) error {
-	s.initOnce.Do(func() {
-		b, err := browser.BuildBrowser(ctx, config.BrowserConfFromENV())
-		if err != nil {
-			s.initErr = err
-			close(s.ready)
-			return
-		}
+// func (s *sessionManager) Init(ctx context.Context) error {
+// 	s.initOnce.Do(func() {
+// 		b, err := browser.BuildBrowser(ctx, config.BrowserConfFromENV())
+// 		if err != nil {
+// 			s.initErr = err
+// 			close(s.ready)
+// 			return
+// 		}
 
-		sess, err := NewRodSession(b, s.eventBus, false)
-		if err != nil {
-			_ = b.Close()
+// 		sess, err := NewRodSession(b, s.eventBus, false)
+// 		if err != nil {
+// 			_ = b.Close()
 
-			s.initErr = err
-			close(s.ready)
-			return
-		}
+// 			s.initErr = err
+// 			close(s.ready)
+// 			return
+// 		}
 
-		s.browser = b
+// 		s.browser = b
 
-		s.sessionMu.Lock()
-		s.session = sess
-		s.sessionMu.Unlock()
+// 		s.sessionMu.Lock()
+// 		s.session = sess
+// 		s.sessionMu.Unlock()
 
-		close(s.ready)
-	})
+// 		close(s.ready)
+// 	})
 
-	return s.initErr
-}
+// 	return s.initErr
+// }
 
-func (s *sessionManager) Session(ctx context.Context) (Session, error) {
-	select {
-	case <-s.ready:
+// func (s *sessionManager) Session(ctx context.Context) (Session, error) {
+// 	select {
+// 	case <-s.ready:
 
-	case <-ctx.Done():
-		return nil, context.Cause(ctx)
-	}
+// 	case <-ctx.Done():
+// 		return nil, context.Cause(ctx)
+// 	}
 
-	if s.initErr != nil {
-		return nil, s.initErr
-	}
+// 	if s.initErr != nil {
+// 		return nil, s.initErr
+// 	}
 
-	s.sessionMu.RLock()
-	defer s.sessionMu.RUnlock()
+// 	s.sessionMu.RLock()
+// 	defer s.sessionMu.RUnlock()
 
-	return s.session, nil
-}
+// 	return s.session, nil
+// }
 
-func (s *sessionManager) Login(ctx context.Context, user User) error {
-	current, err := s.Session(ctx)
-	if err != nil {
-		return err
-	}
+// func (s *sessionManager) Login(ctx context.Context, user User) error {
+// 	current, err := s.Session(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	authenticated, err := NewAuthenticatedSession(
-		ctx,
-		current,
-		s.service,
-		user,
-		s.eventBus,
-	)
-	if err != nil {
-		return err
-	}
+// 	authenticated, err := NewAuthenticatedSession(
+// 		ctx,
+// 		current,
+// 		s.service,
+// 		user,
+// 		s.eventBus,
+// 	)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	s.sessionMu.Lock()
-	s.session = authenticated
-	s.sessionMu.Unlock()
+// 	s.sessionMu.Lock()
+// 	s.session = authenticated
+// 	s.sessionMu.Unlock()
 
-	return nil
-}
+// 	return nil
+// }
 
-func (s *sessionManager) Logout(ctx context.Context) error {
-	current, err := s.Session(ctx)
-	if err != nil {
-		return err
-	}
+// func (s *sessionManager) Logout(ctx context.Context) error {
+// 	current, err := s.Session(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	if err := s.service.Logout(ctx, current); err != nil {
-		return err
-	}
+// 	if err := s.service.Logout(ctx, current); err != nil {
+// 		return err
+// 	}
 
-	root, err := NewRodSession(s.browser, s.eventBus, false)
-	if err != nil {
-		return err
-	}
+// 	root, err := NewRodSession(s.browser, s.eventBus, false)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	s.sessionMu.Lock()
-	old := s.session
-	s.session = root
-	s.sessionMu.Unlock()
+// 	s.sessionMu.Lock()
+// 	old := s.session
+// 	s.session = root
+// 	s.sessionMu.Unlock()
 
-	// The browser remains alive. We only discard the previous session.
-	if old != nil {
-		_ = old.Close(ctx)
-	}
+// 	// The browser remains alive. We only discard the previous session.
+// 	if old != nil {
+// 		_ = old.Close(ctx)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (s *sessionManager) Close() error {
-	<-s.ready
+// func (s *sessionManager) Close() error {
+// 	<-s.ready
 
-	s.sessionMu.RLock()
-	sess := s.session
-	s.sessionMu.RUnlock()
+// 	s.sessionMu.RLock()
+// 	sess := s.session
+// 	s.sessionMu.RUnlock()
 
-	if sess != nil {
-		_ = sess.Close(context.Background())
-	}
+// 	if sess != nil {
+// 		_ = sess.Close(context.Background())
+// 	}
 
-	return s.browser.Close()
-}
+// 	return s.browser.Close()
+// }
 
-func (s *sessionManager) Reconfigure(ctx context.Context, cfg config.BrowserConfig) error {
-	s.stateMu.Lock()
-	defer s.stateMu.Unlock()
+// func (s *sessionManager) Reconfigure(ctx context.Context, cfg config.BrowserConfig) error {
+// 	s.stateMu.Lock()
+// 	defer s.stateMu.Unlock()
 
-	log.Info(s.browserConf, cfg, s.browserConf.Equal(cfg))
+// 	log.Info(s.browserConf, cfg, s.browserConf.Equal(cfg))
 
-	if s.browserConf.Equal(cfg) {
-		return nil
-	}
+// 	if s.browserConf.Equal(cfg) {
+// 		return nil
+// 	}
 
-	if s.session != nil {
-		_ = s.session.Close(ctx)
-		s.session = nil
-	}
+// 	if s.session != nil {
+// 		_ = s.session.Close(ctx)
+// 		s.session = nil
+// 	}
 
-	if s.browser != nil {
-		_ = s.browser.Close()
-		s.browser = nil
-	}
+// 	if s.browser != nil {
+// 		_ = s.browser.Close()
+// 		s.browser = nil
+// 	}
 
-	browser, err := browser.BuildBrowser(ctx, cfg)
-	if err != nil {
-		return err
-	}
+// 	browser, err := browser.BuildBrowser(ctx, cfg)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	session, err := NewRodSession(browser, s.eventBus, false)
-	if err != nil {
-		_ = browser.Close()
-		return err
-	}
+// 	session, err := NewRodSession(browser, s.eventBus, false)
+// 	if err != nil {
+// 		_ = browser.Close()
+// 		return err
+// 	}
 
-	s.browser = browser
-	s.browserConf = cfg
-	s.session = session
+// 	s.browser = browser
+// 	s.browserConf = cfg
+// 	s.session = session
 
-	return nil
-}
+// 	return nil
+// }
