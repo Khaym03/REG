c721cf6f30ef21c4fe14c58adf345e6cde6850e4
Khaym03 <daniellarosa20003@gmail.com>
2026-07-02 19:54:48 -0400

    auth: add context checks and wrap errors in accounts and login-scraper

--- internal/auth/accounts.go
+++ internal/auth/accounts.go
@@ -53,120 +53,176 @@ func (a AccountService) KnownUser(user User) bool {
 
 	if existingUser != nil {
 		// dont need authentication
-		existingUser.LoginState()
-		a.UpdateUser(*existingUser)
-		return true
-	}
+		package auth
 
-	return false
-}
+		import (
+			"context"
+			"fmt"
 
-// Login to check is user exists, the session keeps open,
-// is upto the caller if want to logout inmediatly
-func (a *AccountService) AuthUser(
-	ctx context.Context,
-	user User,
-	session session.Session,
-) (err error) {
+			c "github.com/Khaym03/REG/internal/constants"
+			"github.com/Khaym03/REG/internal/repo"
+			"github.com/Khaym03/REG/internal/session"
+			log "github.com/sirupsen/logrus"
+			"github.com/zalando/go-keyring"
+		)
 
-	var existingUser *RegisterUsers = nil
-
-	users, err := a.p.Load()
-	if err != nil {
-		return err
-	}
-
-	for _, knownUser := range users {
-		if user.Username == knownUser.Username {
-			existingUser = &knownUser
+		type RegisterUsers struct {
+			Username string `json:"username"`
+			LastUse  bool   `json:"last_use"`
+			Logged   bool   `json:"logged"`
 		}
 
-	}
-
-	if existingUser != nil {
-		// dont need authentication
-		existingUser.LoginState()
-		a.UpdateUser(*existingUser)
-		return nil
-	}
-
-	err = a.auth.Login(ctx, session, user)
-	if err != nil {
-		return err
-	}
-
-	// no erros means user exist
-	return a.StoreUserSecret(user)
-}
-
-func (a *AccountService) StoreUserSecret(user User) (err error) {
-	if err = user.Validate(); err != nil {
-		return err
-	}
-
-	err = keyring.Set(c.AppName, user.Username, user.Password)
-	if err != nil {
-		return err
-	}
-
-	users, err := a.p.Load()
-	if err != nil {
-		return err
-	}
-
-	for _, u := range users {
-		u.LastUse = false
-	}
-
-	usr := RegisterUsers{
-		Username: user.Username,
-	}
-
-	usr.LoginState()
-
-	users = append(users, usr)
-
-	return a.p.Save(users)
-}
-
-func (a *AccountService) GetUserPassword(username string) (User, error) {
-	secret, err := keyring.Get(c.AppName, username)
-	if err != nil {
-		return User{}, err
-	}
-
-	return User{Username: username, Password: secret}, nil
-}
-
-func (a *AccountService) GetRegisterUsers() ([]RegisterUsers, error) {
-	return a.p.Load()
-}
-
-func (a *AccountService) UpdateUser(user RegisterUsers) error {
-	old, err := a.p.Load()
-	if err != nil {
-		return err
-	}
-
-	for i, oldUser := range old {
-		if user.Username == oldUser.Username {
-			old[i] = user
+		func (u *RegisterUsers) LoginState() {
+			u.LastUse = true
+			u.Logged = true
 		}
-	}
 
-	return a.p.Save(old)
-}
-
-func (a *AccountService) CurrentUser() *RegisterUsers {
-	users, err := a.p.Load()
-	if err != nil {
-		log.Error(err)
-	}
-
-	for _, u := range users {
-		if u.Logged {
-			return &u
+		type AccountService struct {
+			auth AuthService
+			p    repo.Persistence[[]RegisterUsers]
 		}
-	}
-	return nil
-}
+
+		func NewAccountService(
+			auth AuthService,
+			p repo.Persistence[[]RegisterUsers],
+		) *AccountService {
+			return &AccountService{
+				auth: auth,
+				p:    p,
+			}
+		}
+
+		func (a *AccountService) KnownUser(user User) bool {
+			var existingUser *RegisterUsers
+
+			users, err := a.p.Load()
+			if err != nil {
+				log.Error(err)
+			}
+
+			for i := range users {
+				if user.Username == users[i].Username {
+					existingUser = &users[i]
+					break
+				}
+			}
+
+			if existingUser != nil {
+				existingUser.LoginState()
+				_ = a.UpdateUser(*existingUser)
+				return true
+			}
+
+			return false
+		}
+
+		// Login to check is user exists, the session keeps open,
+		// is upto the caller if want to logout inmediatly
+		func (a *AccountService) AuthUser(
+			ctx context.Context,
+			user User,
+			session session.Session,
+		) (err error) {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+
+			var existingUser *RegisterUsers
+
+			users, err := a.p.Load()
+			if err != nil {
+				return fmt.Errorf("load users: %w", err)
+			}
+
+			for i := range users {
+				if user.Username == users[i].Username {
+					existingUser = &users[i]
+					break
+				}
+			}
+
+			if existingUser != nil {
+				existingUser.LoginState()
+				if err := a.UpdateUser(*existingUser); err != nil {
+					return fmt.Errorf("update user: %w", err)
+				}
+				return nil
+			}
+
+			if err := a.auth.Login(ctx, session, user); err != nil {
+				return fmt.Errorf("auth login: %w", err)
+			}
+
+			if err := a.StoreUserSecret(user); err != nil {
+				return fmt.Errorf("store user secret: %w", err)
+			}
+
+			return nil
+		}
+
+		func (a *AccountService) StoreUserSecret(user User) (err error) {
+			if err = user.Validate(); err != nil {
+				return err
+			}
+
+			if err = keyring.Set(c.AppName, user.Username, user.Password); err != nil {
+				return fmt.Errorf("set keyring secret: %w", err)
+			}
+
+			users, err := a.p.Load()
+			if err != nil {
+				return fmt.Errorf("load users: %w", err)
+			}
+
+			for i := range users {
+				users[i].LastUse = false
+			}
+
+			usr := RegisterUsers{Username: user.Username}
+			usr.LoginState()
+
+			users = append(users, usr)
+
+			return a.p.Save(users)
+		}
+
+		func (a *AccountService) GetUserPassword(username string) (User, error) {
+			secret, err := keyring.Get(c.AppName, username)
+			if err != nil {
+				return User{}, fmt.Errorf("get user password: %w", err)
+			}
+			return User{Username: username, Password: secret}, nil
+		}
+
+		func (a *AccountService) GetRegisterUsers() ([]RegisterUsers, error) {
+			return a.p.Load()
+		}
+
+		func (a *AccountService) UpdateUser(user RegisterUsers) error {
+			old, err := a.p.Load()
+			if err != nil {
+				return err
+			}
+
+			for i, oldUser := range old {
+				if user.Username == oldUser.Username {
+					old[i] = user
+				}
+			}
+
+			return a.p.Save(old)
+		}
+
+		func (a *AccountService) CurrentUser() *RegisterUsers {
+			users, err := a.p.Load()
+			if err != nil {
+				log.Error(err)
+			}
+
+			for i := range users {
+				if users[i].Logged {
+					return &users[i]
+				}
+			}
+			return nil
+		}
--- internal/auth/login-scraper.go
+++ internal/auth/login-scraper.go
@@ -47,36 +47,102 @@ func (l *LoginScraper) Login(
 
 		if err = loginPage.Submit(); err != nil {
 			return
+		package auth
+
+		import (
+			"context"
+			"fmt"
+			"time"
+
+			"github.com/Khaym03/REG/internal/browser"
+			c "github.com/Khaym03/REG/internal/constants"
+			"github.com/Khaym03/REG/internal/event"
+			"github.com/Khaym03/REG/internal/session"
+			"github.com/go-rod/rod"
+			"github.com/mustafaturan/bus/v3"
+		)
+
+		var _ AuthService = (*LoginScraper)(nil)
+
+		type LoginScraper struct {
+			eventBus *bus.Bus
 		}
 
-		return
-	}
-
-	login = browser.WithRetry(ctx, 3, time.Second*10)(login)
-
-	return s.Do(ctx, login)
-}
-
-func (l *LoginScraper) Logout(
-	ctx context.Context,
-	s session.Session,
-) (err error) {
-	logout := func(p *rod.Page) error {
-		err = l.eventBus.Emit(ctx, string(event.Logout), struct{}{})
-		if err != nil {
-			return err
+		func NewLoginScraper(eventBus *bus.Bus) *LoginScraper {
+			return &LoginScraper{eventBus: eventBus}
 		}
 
-		logoutPage := NewLogoutPage(p)
+		func (l *LoginScraper) Login(
+			ctx context.Context,
+			s session.Session,
+			user User,
+		) (err error) {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
 
-		if err = logoutPage.Open(); err != nil {
-			return err
+			login := func(p *rod.Page) (err error) {
+				if err := l.eventBus.Emit(ctx, string(event.Login), struct{}{}); err != nil {
+					return fmt.Errorf("emit login event: %w", err)
+				}
+
+				loginPage := NewLoginPage(p)
+
+				if err := loginPage.Open(); err != nil {
+					return fmt.Errorf("open login page: %w", err)
+				}
+
+				if err := loginPage.EnterCredentials(user); err != nil {
+					return fmt.Errorf("enter credentials: %w", err)
+				}
+
+				if err := loginPage.Submit(); err != nil {
+					return fmt.Errorf("submit login: %w", err)
+				}
+
+				return nil
+			}
+
+			login = browser.WithRetry(ctx, 3, time.Second*10)(login)
+
+			if err := s.Do(ctx, login); err != nil {
+				return fmt.Errorf("session do login: %w", err)
+			}
+
+			return nil
 		}
 
-		return logoutPage.Logout()
-	}
+		func (l *LoginScraper) Logout(
+			ctx context.Context,
+			s session.Session,
+		) (err error) {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
 
-	logout = browser.WithRetry(ctx, 3, c.DefaultTimeout)(logout)
+			logout := func(p *rod.Page) error {
+				if err := l.eventBus.Emit(ctx, string(event.Logout), struct{}{}); err != nil {
+					return fmt.Errorf("emit logout event: %w", err)
+				}
 
-	return s.Do(ctx, logout)
-}
+				logoutPage := NewLogoutPage(p)
+
+				if err := logoutPage.Open(); err != nil {
+					return fmt.Errorf("open logout page: %w", err)
+				}
+
+				if err := logoutPage.Logout(); err != nil {
+					return fmt.Errorf("perform logout: %w", err)
+				}
+
+				return nil
+			}
+
+			logout = browser.WithRetry(ctx, 3, c.DefaultTimeout)(logout)
+
+			if err := s.Do(ctx, logout); err != nil {
+				return fmt.Errorf("session do logout: %w", err)
+			}
+
+			return nil
+		}
